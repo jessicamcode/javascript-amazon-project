f36b9697a49cace1c88058be14396cbcb9596b15
jessicamcode <molinajessica.b15@gmail.com>
2026-06-15 16:27:28 -0400

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -65,12 +65,18 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 		if (matchingItem) {
 			matchingItem.quantity += 1;
 		} else {
+			cart.push({
+				productId: productId,
+				quantity: 1,
+			});
 		}
-		cart.push({
-			productId: productId,
-			quantity: 1,
+
+		let cartQuantity = 0;
+
+		cart.forEach((item) => {
+			cartQuantity += item.quantity;
 		});
 
-		console.log(cart);
+		document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 	});
 });
